36d821bc2ca803d5708840f9310072f660c1efcf
Mahi07 <mahidakshana@gmail.com>
2020-07-26 23:30:52 +0530

    check

---
+++ app/src/main/java/com/example/karakoram/activity/uploadActivity.java
@@ -0,0 +1,14 @@
+package com.example.karakoram.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}
