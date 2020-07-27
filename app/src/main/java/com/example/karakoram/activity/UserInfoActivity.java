e61f98373e7db631fede5c590dedc6b17bda7621
sauravmittal001 <sauravmittal001@gmail.com>
2020-07-28 00:54:55 +0530

    designed UI of user page

--- app/src/main/java/com/example/karakoram/activity/UserInfoActivity.java
+++ app/src/main/java/com/example/karakoram/activity/UserInfoActivity.java
@@ -1,39 +1,77 @@
 package com.example.karakoram.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.karakoram.R;
+import com.example.karakoram.resource.User;
+import com.example.karakoram.views.CustomSpinner;
+import com.example.karakoram.views.CustomSpinnerAdapter;
+
+import java.util.Objects;
 
 public class UserInfoActivity extends AppCompatActivity {
-    private EditText wing_edittext;
-    private EditText room_edittext;
+
+    private CustomSpinner floorSpinner, roomNumberSpinner, wingSpinner;
+    private String[] floor, roomNumber, wing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            Objects.requireNonNull(this.getSupportActionBar()).hide();
+        } catch (NullPointerException ignored) {
+        }
         setContentView(R.layout.activity_user_info);
+        initVariables();
+        initViews();
+        setViews();
 
-        wing_edittext=(EditText)findViewById(R.id.wing_edittext);
-        room_edittext=(EditText)findViewById(R.id.room_edittext);
     }
 
-    public void back(View view)
-    {
+    private void initVariables() {
+        floor = getResources().getStringArray(R.array.floor);
+        roomNumber = getResources().getStringArray(R.array.room_number);
+        wing = getResources().getStringArray(R.array.wing);
+    }
+
+    private void initViews() {
+        floorSpinner = findViewById(R.id.spinner_floor);
+        roomNumberSpinner = findViewById(R.id.spinner_room_number);
+        wingSpinner = findViewById(R.id.spinner_wing);
+    }
+
+    private void setViews() {
+        CustomSpinnerAdapter floorAdapter = new CustomSpinnerAdapter(this, R.layout.spinner_item, floor);
+        floorAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        floorSpinner.setAdapter(floorAdapter);
+
+        CustomSpinnerAdapter roomNumberAdapter = new CustomSpinnerAdapter(this, R.layout.spinner_item, roomNumber);
+        roomNumberAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        roomNumberSpinner.setAdapter(roomNumberAdapter);
+
+        CustomSpinnerAdapter wingAdapter = new CustomSpinnerAdapter(this, R.layout.spinner_item, wing);
+        wingAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        wingSpinner.setAdapter(wingAdapter);
+    }
+
+
+    public void back(View view) {
         super.onBackPressed();
     }
-    public void save(View view)
-    {
-        if(room_edittext.getText().toString().isEmpty()||wing_edittext.getText().toString().isEmpty())
-        {
-            Toast.makeText(getApplicationContext(),"Enter the data",Toast.LENGTH_SHORT).show();
-        }
-       else{
-           Toast.makeText(getApplicationContext(),"Wing -  "+wing_edittext.getText().toString() +"\n"+"room no. -"+room_edittext.getText().toString(),Toast.LENGTH_SHORT).show();
-        }
+
+    public void save(View view) {
+        String selectedRoom = floor[floorSpinner.getSelectedItemPosition()] + "-" + roomNumber[roomNumberSpinner.getSelectedItemPosition()];
+        String selectedWing = wing[wingSpinner.getSelectedItemPosition()];
+        SharedPreferences sharedPreferences = getSharedPreferences(User.SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("room", selectedRoom);
+        editor.putString("wing", selectedWing))
+        editor.apply();
     }
+
+
 }
