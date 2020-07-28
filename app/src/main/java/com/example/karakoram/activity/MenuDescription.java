31fb4c2a74f7629b99fac0785196a1c68fcabf8e
sauravmittal001 <sauravmittal001@gmail.com>
2020-07-29 01:49:10 +0530

    Minor change in event description

---
+++ app/src/main/java/com/example/karakoram/activity/MenuDescription.java
@@ -0,0 +1,13 @@
+package com.example.karakoram.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_description);
+    }
+}
