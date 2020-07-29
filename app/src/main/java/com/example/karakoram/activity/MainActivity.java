564efb9c57a585a6d65571d505e37e13fc7d9a09
cant12 <veeramakali1@gmail.com>
2020-07-29 16:31:03 +0530

    styling of navbars and tabs

--- app/src/main/java/com/example/karakoram/activity/MainActivity.java
+++ app/src/main/java/com/example/karakoram/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.karakoram.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +23,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
