fe1debaa6ebecf2c9c7cd0736522b14f327b6ae8
cant12 <veeramakali1@gmail.com>
2020-08-06 00:34:31 +0530

    minor changes

--- app/src/main/java/com/example/karakoram/activity/AboutActivity.java
+++ app/src/main/java/com/example/karakoram/activity/AboutActivity.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 
 import com.example.karakoram.R;
 
+import java.util.Objects;
+
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        try {
+            Objects.requireNonNull(this.getSupportActionBar()).hide();
+        } catch (NullPointerException ignored) {
+        }
     }
 }
